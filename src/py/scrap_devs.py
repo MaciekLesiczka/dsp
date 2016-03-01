8d014e1b179f033014811ff3db8d239ab3198a7b
macieklesiczka <maciek.lesiczka@gmail.com>
2016-03-01 20:10:26 +0100

    scraping fix

--- src/py/scrap_devs.py
+++ src/py/scrap_devs.py
@@ -1,6 +1,6 @@
 import urllib2
 from bs4 import BeautifulSoup
-from pandas import DataFrame as pd
+from pandas import DataFrame
 
 page = urllib2.urlopen('http://www.maciejaniserowicz.com/daj-sie-poznac/uczestnicy/').read()
 soup = BeautifulSoup(page)
@@ -12,17 +12,17 @@ def get_dev_info(hr_element):
     header = hr_element.find_next_sibling()
     details = header.find_next_sibling()
     links = details.find_all('a')
+    header_html = unicode(header)
     dev = {'name': header.text[0:(header.text.find("-") - 1)],
-           'project_title': header.text[header.text.find('"') + 1:(header.text.find('(') - 2)],
-           "tech_stack": header.text[header.text.find("(") + 1:header.text.find(")")],
+           'project_title': header_html[header_html.find('"<i>') + 4:(header_html.find('</i>"'))],
+           "tech_stack": header_html[header_html.find('</i>" (') + 7:header_html.find(")</h4>")],
            "project_desc": details.find("span").text,
            "blog_url": links[0]['href'],
            "repo_url": links[1]['href']}
     return dev
 
-
 devs = [get_dev_info(hr) for hr in hrs]
 
-df = pd(devs)
+df = DataFrame(devs)
 
 df.to_csv("..\..\data\devs.csv", encoding="utf-8", index=False)
