b6f08ea6633be4e3df1ebae7c56de3905cb3b854
macieklesiczka <maciek.lesiczka@gmail.com>
2016-03-05 20:19:50 +0100

    scrap repo name and owner

--- src/py/scrap_devs.py
+++ src/py/scrap_devs.py
@@ -20,11 +20,56 @@ def get_dev_info(hr_element):
            "tech_stack": get_cell('.dsp_tech'),
            "project_desc": get_cell('.dsp_prj_desc'),
            "blog_url": contestant_div.select('.dsp_blog_url')[0]['href'],
-           "repo_url": contestant_div.select('.dsp_src_url')[0]['href']}
+           "repo_url": contestant_div.select('.dsp_src_url')[0]['href'],
+           }
     return dev
 
 devs = [get_dev_info(hr) for hr in hrs]
 
+
+def get_repo_info(repo_url):
+    repo_url = repo_url.lower()
+    if repo_url.endswith("/"):
+        repo_url = repo_url[:-1]
+
+    if repo_url.endswith(".git"):
+        repo_url = repo_url[:-4]
+    if repo_url.startswith('https://github.com/'):
+        repo_url = repo_url.replace('https://github.com/','')
+    elif repo_url.startswith('http://github.com/'):
+        repo_url = repo_url.replace('http://github.com/','')
+    else:
+        repo_url = None
+    repo_owner = None
+    repo_name = None
+    if repo_url is not None:
+        repo_parts = repo_url.split('/')
+
+        if len(repo_parts) > 0:
+            repo_owner = repo_parts[0]
+            if len(repo_parts) == 2:
+                repo_name = repo_parts[1]
+            elif len(repo_parts) == 1:
+                #exceptions
+                if repo_owner == "konstruktywnie":
+                    repo_name = "metod"
+                if repo_owner in ["meteostation", "wikibus"]:
+                    repo_name = "/all/"
+    return {
+                'repo_owner':repo_owner,
+                'repo_name':repo_name
+    }
+
 df = DataFrame(devs)
+
+parts = df.repo_url.map(lambda x: get_repo_info(x))
+df['repo_owner'] = parts.map(lambda x: x["repo_owner"])
+df['repo_name'] = parts.map(lambda x: x["repo_name"])
+
+not_found_repos = df[df.repo_name.isnull()][['blog_url', 'repo_url']]
+
+if len(not_found_repos) > 0:
+    print 'not found repos for:'
+    print not_found_repos
 
 df.to_csv("..\..\data\devs.csv", encoding="utf-8", index=False)
