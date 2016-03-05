86c92b529840dc30714d38459de8841a16487670
macieklesiczka <maciek.lesiczka@gmail.com>
2016-03-05 21:58:09 +0100

    add extracting repositories script and data

---
+++ src/r/extract_repos.R
@@ -0,0 +1,37 @@
+library(github)
+
+client.id <- "86293fe8761fdc158441" 
+client.secret<- "8e963a81e3067e32c3647d94973417b8ef1b64a3" 
+ctx = interactive.login(client.id, client.secret)
+
+
+devs = read.csv("../../data/devs.csv", encoding = "UTF-8")
+
+
+all.org.repos = lapply(as.character(subset(devs,repo_name=="/all/")$repo_owner), function(owner){
+  reps_request = get.organization.repositories(owner)
+  
+  if(reps_request$code != 200 && reps_request$code != 304){
+    print(paste("error for",owner,reps_request$code))
+    NULL
+  }
+  else{
+    names = sapply(reps_request$content, function(r) r$name)
+    list(rep(owner,length(names)),names)
+    
+  }
+})
+
+all.org.repos.df = data.frame(
+  repo_owner=unlist(sapply(all.org.repos, function(x)x[1])),
+  repo_name=unlist(sapply(all.org.repos, function(x)x[2]))
+)
+
+
+all.repos = subset(subset(devs,repo_name!="/all/"), repo_name!="")[, c("repo_owner","repo_name")]
+
+repos = rbind(all.repos,all.org.repos.df)
+
+
+
+write.csv(repos, "../../data/repos.csv")
