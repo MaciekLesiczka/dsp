6f3d857318aca626a1122a0fbc03adbe7b1033ac
macieklesiczka <maciek.lesiczka@gmail.com>
2016-04-07 08:49:20 +0200

    add first version of barchart

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 var d3 = require('d3')
-d3.csv('./data/sample.csv', function(data){
-	d3.select('#container').selectAll('circle')
-	  .data(data)
-      .enter()
-      .append('circle')
-	  .attr('cx',function(d){return d.k})
-	  .attr('cy',function(d){return d.v})
-	  .attr('r',8)   	  
-	  .transition()
-	  .delay(300)
-	  .duration(1000)
-	  .attr('cy',80)
-})
 
+// web  mobile     iot    game    tool    data windows 
+// 121      46      30      50      53      16      28 
+
+var categories = [
+   {name:'web', count: 121},
+   {name:'mobile', count: 46},
+   {name:'iot', count: 30},
+   {name:'game', count: 50},
+   {name:'tool', count: 53},
+   {name:'data', count: 16},
+   {name:'windows', count: 28}
+]
+
+
+d3.select('#container')
+  .selectAll('rect')
+  .data(categories)
+  .enter()
+  .append('rect')
+  .attr('x',10)
+  .attr('y',function(_,i){return i*10})
+  .attr('height',8)
+  .attr('width',function(d){return d.count; })
