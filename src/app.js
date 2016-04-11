ff228de63be4c657ad4f64abb3ff1b6e1a95d050
macieklesiczka <maciek.lesiczka@gmail.com>
2016-04-11 08:42:15 +0200

    first chart version

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-var d3 = require('d3')
+d3 = require('d3')
 
 // web  mobile     iot    game    tool    data windows 
 // 121      46      30      50      53      16      28 
+
 
 var categories = [
    {name:'web', count: 121},
@@ -13,13 +14,25 @@ var categories = [
    {name:'windows', count: 28}
 ]
 
+var padding ={
+    top:10,
+    left:10
+}
+
+var options = {
+    bar :{
+        size:8,
+        distance:10
+    },    
+}
 
 d3.select('#container')
   .selectAll('rect')
   .data(categories)
   .enter()
   .append('rect')
-  .attr('x',10)
-  .attr('y',function(_,i){return i*10})
-  .attr('height',8)
+  .attr('x',padding.left)
+  .attr('y',function(_,i){return i*options.bar.distance + padding.top})
+  .attr('height',options.bar.size)
   .attr('width',function(d){return d.count; })
+  .style('fill', "#bdbdbd")
