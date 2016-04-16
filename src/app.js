428833ef2926c63a8427d2e3e2c4c69586b2dbfd
macieklesiczka <maciek.lesiczka@gmail.com>
2016-04-16 12:36:15 +0200

    barchart2 all changes

--- src/app.js
+++ src/app.js
@@ -1,32 +1,62 @@
-var d3 = require('d3')
-
-var padding ={
-    top:10,
-    left:10
-}
+d3 = require('d3')
 
 var options = {
-    bar :{
-        size:10,
-        distance:14,
-        color:'#bdbdbd'
-    },    
-}
+    bar :{                
+        color:'#bdbdbd',
+        padding:0.25
+    },
+    width:400,
+    height:400,
+    margin:{
+        left:100
+    }
+}    
 
-d3.csv('data/categories.csv', function(categories){
+options.barMaxSize = options.width - options.margin.left
+
+d3.csv('data/categories.csv',
+ function(d){
+     d.count = parseInt(d.count)
+     return d;
+ },
+ function(categories){    
+    categories = categories.sort(function(a, b) { return d3.descending(a.count, b.count); });
+            
+    var xScale =
+        d3.scale.linear()
+                .domain([0,d3.max(categories.map(function(x){return x.count}))])
+                .range([0,options.barMaxSize])     
+    console.log()
+    var yScale = 
+        d3.scale.ordinal()
+                .domain(categories.map(function(x){return x.category}))
+                .rangeRoundBands([0,options.height],options.bar.padding)    
+    var yAxis = d3.svg.axis().scale(yScale).orient('left')        
+    var bar = d3.select('#container')
+      .selectAll('rect')
+      .data(categories)
+      .enter()
+      .append('g')
+      .attr('transform', 'translate(' + options.margin.left + ',' + '0)')
     
+    bar.append('rect')
+       .attr('y',function(x){return yScale(x.category)})
+       .attr('height',yScale.rangeBand())
+       .attr('width',function(d){return xScale(d.count); })              
+       .style('fill',options.bar.color)
+      
+    bar.append('text')
+       .classed('number',true)
+       .attr('y',function(x){return yScale(x.category)+yScale.rangeBand()/2})
+       .attr('x',function(d){return xScale(d.count)-3; })
+       .attr('height',yScale.rangeBand()/2)              
+       .attr('dy','.35em')
+       .text(function(d){return d.count});
+       
     d3.select('#container')
-    .selectAll('rect')
-    .data(categories)
-    .enter()
-    .append('rect')
-    
-    .attr('y',function(_,i){return i*options.bar.distance + padding.top
-        })
-    .attr('height',options.bar.size)
-    .attr('width',function(d){return d.count; })
-    .attr('x',padding.left)
-    .style('fill',options.bar.color)    
+      .append('g')
+      .attr('transform', 'translate(' + options.margin.left + ',' + '0)')
+      .call(yAxis)            
 })
 
 
