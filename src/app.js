cf15f60975ef31b1ef2542f16d952ded36fff271
macieklesiczka <maciek.lesiczka@gmail.com>
2016-04-24 09:49:28 +0200

    add title and jetpack

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-d3 = require('d3')
+var d3 = require('d3-jetpack')
+
 
 var options = {
     bar :{                
         color:'#6baed6',
-        padding:0.25
+        padding:0.3
     },
-    width:400,
-    height:400,
+    width:450,
+    height:350, 
     margin:{
-        left:100
+        left:100,
+        top:30
     }
 }    
 
-options.barMaxSize = options.width - options.margin.left
+options.barWidth = options.width - options.margin.left
+options.barHeight = options.height - options.margin.top
 
 d3.csv('data/categories.csv',
  function(d){
@@ -20,24 +23,27 @@ d3.csv('data/categories.csv',
      return d;
  },
  function(categories){    
-    categories = categories.sort(function(a, b) { return d3.descending(a.count, b.count); });
+    categories.sort(d3.descendingKey('count'));
             
     var xScale =
         d3.scale.linear()
                 .domain([0,d3.max(categories.map(function(x){return x.count}))])
-                .range([0,options.barMaxSize])     
-    console.log()
+                .range([0,options.barWidth])     
     var yScale = 
         d3.scale.ordinal()
                 .domain(categories.map(function(x){return x.category}))
-                .rangeRoundBands([0,options.height],options.bar.padding)    
-    var yAxis = d3.svg.axis().scale(yScale).orient('left')        
-    var bar = d3.select('#container')
+                .rangeRoundBands([0,options.barHeight],options.bar.padding)    
+    var yAxis = d3.svg.axis().scale(yScale).orient('left')   
+    
+    var chartGroup = d3.select('.chart')
+      .append('g')
+      .attr('transform', 'translate(' + options.margin.left + ',' + options.margin.top + ')')
+         
+    var bar =chartGroup
       .selectAll('rect')
       .data(categories)
       .enter()
-      .append('g')
-      .attr('transform', 'translate(' + options.margin.left + ',' + '0)')
+      .append('g')      
     
     bar.append('rect')
        .attr('y',function(x){return yScale(x.category)})
@@ -49,14 +55,19 @@ d3.csv('data/categories.csv',
        .classed('number',true)
        .attr('y',function(x){return yScale(x.category)+yScale.rangeBand()/2})
        .attr('x',function(d){return xScale(d.count)-3; })
-       .attr('height',yScale.rangeBand()/2)              
+                     
        .attr('dy','.35em')
        .text(function(d){return d.count});
        
-    d3.select('#container')
-      .append('g')
-      .attr('transform', 'translate(' + options.margin.left + ',' + '0)')
-      .call(yAxis)            
+    chartGroup.call(yAxis)      
+    
+    d3.select('.chart')         
+        .append('text')
+        .classed('title',true)
+        .text('DSP \'16 Projects' )        
+        .attr('x', 5)
+        .attr('y', options.margin.top/2)
+        .attr('dy','.35em')                    
 })
 
 
