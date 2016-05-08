c1cdf60b1f3cf60faaee46f8ee3912c29d3ef78e
macieklesiczka <maciek.lesiczka@gmail.com>
2016-05-08 09:46:11 +0200

    remove es6 syntax

--- stats/bundle.js
+++ stats/bundle.js
@@ -6,12 +6,12 @@ reductio = require('reductio')
  
 var blogReductio = function () {
     return reductio()
-    .filter(x=> x.type === 'blog') 
+    .filter(function(x){return x.type === 'blog'}) 
 }
 
 var projReductio = function () {
     return reductio()
-    .filter(x=> x.type === 'proj') 
+    .filter(function(x){return x.type === 'proj'}) 
 }
 
 var projectReducer = projReductio()
@@ -26,7 +26,7 @@ var blogReducer = blogReductio()
     .exceptionCount(true) 
  
 d3.csv('20160507_all_events.csv', function(data){    
-    var getDate = d => new Date(d.getFullYear() , d.getMonth(), d.getDate())    
+    var getDate = function(d) {return new Date(d.getFullYear() , d.getMonth(), d.getDate())}    
     var parseDate = d3.time.format("%Y-%m-%d").parse;
     data.forEach(function(d) {
         d.date = getDate( parseDate(d.date));        
